686f1268cc6f0a9fef94f86046fabe661f12cb05
BlueSky23 <liupeipeng@gmail.com>
2019-12-11 17:21:43 +0800

    My Solution

---
+++ P26：RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        length = len(nums)
+        cnt = 0
+        while cnt < length:
+            # 有相同的元素，删除，并更新数组长度
+            if cnt - 1 >= 0 and nums[cnt] == nums[cnt - 1]:
+                nums.pop(cnt)
+                length -= 1
+            else:
+                cnt += 1
+        return length
