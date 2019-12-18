7d17462ebd9b3f354351a2e350039de31058a006
BlueSky23 <liupeipeng@gmail.com>
2019-12-18 08:53:47 +0800

    My Solution——组合和2，效率很低

---
+++ 整数相关/P40：CombinationSum2.py
@@ -0,0 +1,31 @@
+# 思路：当前元素只有两种可能，选择或不选择，继而考虑后续子问题的解
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+         # 目标值不大于0
+        if target <= 0:
+            return
+        # 数组为空
+        if not candidates:
+            return
+
+        rs = []
+        # 每一个元素都只有两种可能，出现（1）或不出现（0）
+        for i in range(2):
+            # 当前元素为target值
+            if target - i * candidates[0] == 0:
+                rs.append([candidates[0]])
+                continue
+            tmp = self.combinationSum2(candidates[1:], target - i * candidates[0])
+            if tmp:
+                # 对子问题的每一个解判断是否加入当前元素
+                for comb in tmp:
+                    # 加入或不加入当前元素candidates[0]
+                    for j in range(i):
+                        comb.append(candidates[0])
+                    rs.append(comb)
+        # 去重
+        if rs:
+            rs = [tuple(sorted(item)) for item in rs]
+            rs = list(set(rs))
+            rs = [list(item) for item in rs]
+        return rs
