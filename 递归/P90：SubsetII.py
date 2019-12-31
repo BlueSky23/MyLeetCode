1b82134f0d8365cd9366d6075a1f8a185a2e17d1
BlueSky23 <liupeipeng@gmail.com>
2019-12-31 09:53:14 +0800

    My Solution——有重复元素的超子集

---
+++ 递归/P90：SubsetII.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsetsWithDup(self, nums):
+        if not nums:
+            return [[]]
+        # 转化为子问题求解
+        tmp = self.subsetsWithDup(nums[1:])
+        # 保存结果
+        ret = []
+        ret.extend(tmp)
+        # 在子问题的每一个解中添加当前元素
+        for i in tmp:
+            ls = []
+            ls.extend(i)
+            ls.append(nums[0])
+            ls.sort()
+            if ls not in ret:
+                ret.append(ls)
+
+        return ret
