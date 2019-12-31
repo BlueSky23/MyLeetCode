dc055029b94b93ed5d56c6c8c8eaefc1bdd5c585
bluesky23 <liupeipeng@gmail.com>
2019-12-31 22:37:46 +0800

    My solution——递归

---
+++ 递归/P87：ScrambleString.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2):
+            return False
+        if len(s1) <= 2:
+            return s1 == s2 or s1 == s2[::-1]
+        # 找到分割点，有两种可能，s1的前、后部分分别与s2的前、后部分对应，
+        # 或者s1的前、后部分分别与s2的后、前部分对应
+        for i in range(1, len(s1)):
+            pre1, post1 = s1[:i], s1[i:]
+            pre2, post2 = s2[:i], s2[i:]
+            # 只有对应部分的字母个数都相同时再去判断
+            if Counter(pre1) == Counter(pre2) and Counter(post1) == Counter(post2):
+                if self.isScramble(pre1, pre2) and self.isScramble(post1, post2):
+                    return True
+            pre2, post2 = s2[:len(s1) - i], s2[len(s1) - i:]
+            if Counter(pre1) == Counter(post2) and Counter(post1) == Counter(pre2):
+                if self.isScramble(pre1, post2) and self.isScramble(post1, pre2):
+                    return True
+
+        return False
