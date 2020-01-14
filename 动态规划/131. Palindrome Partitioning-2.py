c75da6017a74a28f080a31e46016f2b0adf3f7f1
BlueSky23 <liupeipeng@gmail.com>
2020-01-14 19:15:19 +0800

    My Solution——动态规划

---
+++ 动态规划/131. Palindrome Partitioning-2.py
@@ -0,0 +1,20 @@
+# 动态规划
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        # 判断是否是回文
+        def isPalindrome(s):
+            return s == s[::-1]
+
+        if not s:
+            return [[]]
+
+        dp = [[] for _ in range(len(s) + 1)]
+        dp[len(s)].append([])
+        # 由子问题构造父问题
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i + 1, len(s) + 1):
+                if isPalindrome(s[i:j]):
+                    for tmp in dp[j]:
+                        dp[i].append([s[i:j]] + tmp)
+
+        return dp[0]
