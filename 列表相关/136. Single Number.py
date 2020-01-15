8b85be10a22e02ef71960afb28be5402f43ae1ac
BlueSky23 <liupeipeng@gmail.com>
2020-01-15 14:58:56 +0800

    My Solution——使用额外空间

---
+++ 列表相关/136. Single Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        s = set()
+        for i in range(len(nums)):
+            if nums[i] not in s:
+                s.add(nums[i])
+            else:
+                s.remove(nums[i])
+
+        return s.pop()
