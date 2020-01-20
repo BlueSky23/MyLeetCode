abad57d530047a1706b4807e78346de4d35e3966
BlueSky23 <liupeipeng@gmail.com>
2020-01-20 14:33:14 +0800

    My Solution——递归后序遍历

---
+++ 树相关/145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        def helper(root, ret):
+            if not root:
+                return
+            helper(root.left)
+            helper(root.right)
+            ret.append(root.val)
+
+        ret = []
+        helper(root, ret)
+
+        return ret
