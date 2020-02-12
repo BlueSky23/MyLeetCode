5575b7683e3929f6145de90f6e58379839e757b3
BlueSky23 <liupeipeng@gmail.com>
2020-02-12 16:14:06 +0800

    My Solution——O(logn)复杂度

--- 列表相关/162. Find Peak Element-2.py
+++ 列表相关/162. Find Peak Element-2.py
@@ -13,7 +13,7 @@ class Solution:
         i, j = 1, len(nums) - 2
         while i <= j:
             mid = (i + j) // 2
-            # 总会有解，所以这是出口
+            # 总会有解，所以这一定是出口
             if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
                 return mid
             elif nums[mid] < nums[mid - 1] and nums[mid] < nums[mid + 1]:
