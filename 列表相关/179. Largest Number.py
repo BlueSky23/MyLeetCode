34b5d62ca220b48f92cbf4c2c173ab9ce6ffa080
BlueSky <liupeipeng@gmail.com>
2020-12-02 11:23:46 +0800

    Largest number

---
+++ 列表相关/179. Largest Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(i) for i in nums]
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if int(nums[j] + nums[i]) >= int(nums[i] + nums[j]):
+                    nums[i], nums[j] = nums[j], nums[i]
+
+        return str(int(''.join(nums)))
