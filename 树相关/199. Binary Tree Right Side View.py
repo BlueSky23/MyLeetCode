33ec2692a791c4d4e0d7ec6ffb53fbe6563a4b40
BlueSky <liupeipeng@gmail.com>
2020-12-03 11:49:55 +0800

    Level tranverse中怎么获取每一层的最后一个元素

--- 树相关/199. Binary Tree Right Side View.py
+++ 树相关/199. Binary Tree Right Side View.py
@@ -9,22 +9,15 @@ class Solution:
         if not root:
             return []
         vals = []
-        parents_num, children_num = 1, 0
         nodes = [root]
         while len(nodes) != 0:
-            node = nodes.pop(0)
-            parents_num -= 1
-
-            if node.left:
-                nodes.append(node.left)
-                children_num += 1
-            if node.right:
-                nodes.append(node.right)
-                children_num += 1
-
-            if parents_num == 0:
-                vals.append(node.val)
-                parents_num = children_num
-                children_num = 0
+            vals.append(nodes[-1].val)
+            size = len(nodes)
+            for i in range(size):
+                node = nodes.pop(0)
+                if node.left:
+                    nodes.append(node.left)
+                if node.right:
+                    nodes.append(node.right)
 
         return vals
